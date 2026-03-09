dc8ab3cac1abf3e4a4b9d0ee77fc2ab9398c8a0e
Viterkim <vkc@connectedcars.dk>
2026-03-09 14:40:59 +0100

    polish.lua: fix mouse scroll speed on linux

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,15 +1,14 @@
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
-
 vim.cmd "set winblend=0"
 
--- Remove unwanted formatoptions globally
+-- Remove unwanted format options globally
 vim.opt.formatoptions:remove { "c", "r", "o" }
 
--- Ensure formatoptions are enforced each time you open or switch to a buffer
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "BufReadPost", "BufNewFile" }, {
   callback = function() vim.opt.formatoptions:remove { "c", "r", "o" } end,
-  pattern = "*", -- Applies to all files
+  pattern = "*",
   desc = "Remove auto-commenting and other format options for cleaner pasting",
 })
+
+-- Increase scroll speed with mouse, only on linux (gg wayland)
+local os = (vim.uv or vim.loop).os_uname().sysname
+if os == "Linux" then vim.opt.mousescroll = "ver:8,hor:2" end
