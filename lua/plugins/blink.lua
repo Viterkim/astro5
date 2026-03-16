5456265e23fc16e5604cc4cbc99825944f7b6337
Viterkim <vkc@connectedcars.dk>
2026-03-16 15:04:07 +0100

    blink.lua: fix auto complete

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,16 +2,23 @@ return {
   "Saghen/blink.cmp",
   opts = function(_, opts)
     opts.keymap = vim.tbl_extend("force", opts.keymap or {}, {
-      ["<Down>"] = { "insert_next", "fallback" },
-      ["<Up>"] = { "insert_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<Up>"] = { "select_prev", "fallback" },
       ["<CR>"] = { "accept", "fallback" },
-      -- stop accidentally hiding completion
       ["<C-e>"] = false,
     })
 
     opts.completion = vim.tbl_deep_extend("force", opts.completion or {}, {
       menu = {
         auto_show = true,
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+      trigger = {
+        show_on_insert = false,
+        show_on_insert_on_trigger_character = false,
       },
     })
 
